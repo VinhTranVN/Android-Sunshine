06059081dba270cc1409286304991d4b9d180133
Vinh Tran <thevinhit2009@gmail.com>
2016-03-14 00:01:39 +0700

    add source

---
+++ app/src/androidTest/java/vinhtt/example/android_sunshine/ApplicationTest.java
@@ -0,0 +1,13 @@
+package vinhtt.example.android_sunshine;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
