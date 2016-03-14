466f5ca9fa94fb9627752e52cb3b776b76fded39
Vinh Tran <vit@tapptic.com>
2016-03-14 18:34:58 +0700

    add hardcode list weather

--- app/src/main/java/vinhtt/example/android_sunshine/MainActivityFragment.java
+++ app/src/main/java/vinhtt/example/android_sunshine/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,5 +20,28 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Get ListView object from xml
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+
+        // Defined Array weathers to show in ListView
+        String[] weathers = new String[] {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Sunny - 88/63",
+                "Thu - Sunny - 88/63",
+                "Fri - Sunny - 88/63",
+                "Sat - Sunny - 88/63",
+                "Sun - Sunny - 88/63"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, weathers);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
     }
 }
